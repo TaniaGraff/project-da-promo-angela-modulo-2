d82d1ec257d243aa7492d90efc6fd9e928224cbe
Tania <161022301+TaniaGraff@users.noreply.github.com>
2024-05-11 16:20:37 +0200

    Añadida a la tabla oscars los datos, manualmente.

--- Schema_CinemExtract.sql
+++ Schema_CinemExtract.sql
@@ -14,6 +14,8 @@ mejor_actriz VARCHAR(200) NOT NULL,
 id_actiz INT NOT NULL,
 PRIMARY KEY (id_ceremonia));
 
+DROP table oscars3;
+
 CREATE table oscars2(
 id_ceremonia INT NOT NULL AUTO_INCREMENT,
 año_ceremonia INT NOT NULL,
@@ -23,4 +25,11 @@ mejor_actor VARCHAR(200) NOT NULL,
 mejor_actriz VARCHAR(200) NOT NULL,
 PRIMARY KEY (id_ceremonia));
 
-DROP table oscars2; 
+INSERT INTO oscars2(año_ceremonia, mejor_pelicula, mejor_director, mejor_actor, mejor_actriz)
+VALUES 
+(2015, ' Birdman or (The Unexpected Virtue of Ignorance)', ' A. G. Inarritu', 'E. Redmayne',' J. Moore'),
+(2016, ' Spotlight', ' A. G. Inarritu', 'L. DiCaprio', ' B. Larson'),
+(2017, ' Moonlight', ' D. Chazelle', 'C. Affleck', ' E. Stone'),
+(2018, ' The Shape of Water', ' G. del Toro', 'G. Oldman', ' F. McDormand'),
+(2019, ' Green Book', ' A. Cuaron', 'R. Malek', ' O. Colman'),
+(2020, ' Parasite', ' Bong J.', 'J. Phoenix', ' R. Zellweger');
