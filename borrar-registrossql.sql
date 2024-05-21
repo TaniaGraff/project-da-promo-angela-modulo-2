7db2e705e6ead175d6d05bba6cada13d837b750f
Franca Tortarolo <francantortarolo@gmail.com>
2024-05-21 21:04:34 +0200

    queries para ver duplicaods y eliminar

--- borrar-registrossql.sql
+++ borrar-registrossql.sql
@@ -7,3 +7,16 @@ WHERE ID NOT IN (SELECT id_pelicula FROM detalles_peliculas);
 
 DELETE FROM peliculas_generos
 WHERE (nombre columna) = "...";
+
+
+SELECT *
+FROM peliculas_generos
+WHERE ID = 'tt0478970';
+
+
+-- buscar duplicaods
+
+SELECT titulo, COUNT(titulo)
+FROM detalles_peliculas
+GROUP BY titulo
+HAVING COUNT(titulo) > 1;
